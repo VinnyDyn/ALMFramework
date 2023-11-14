41bc73a7576b4506b2f9b7dad54106d08f7524c6
Vinicius Basile <vinicius.basile@fellowmind.dk>
2023-11-14 12:36:42 -0300

    build.yaml

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -1,41 +1,50 @@
 function Invoke-Clone-Or-Sync-Solution {
     param (
-        [Parameter(Mandatory)] [String]$pacPath,
-        [Parameter(Mandatory)] [String]$tenantId,
-        [Parameter(Mandatory)] [String]$clientId,
-        [Parameter(Mandatory)] [String]$clientSecret,
-        [Parameter(Mandatory)] [String]$url,
-        [Parameter(Mandatory)] [String]$solutionFolder,
-        [Parameter(Mandatory)] [String]$solution
+        [Parameter(Mandatory = $true)][Alias("p")][string]$pacPath,
+        [Parameter(Mandatory = $true)][Alias("t")][string]$tenantId,
+        [Parameter(Mandatory = $true)][Alias("c")][string]$clientId,
+        [Parameter(Mandatory = $true)][Alias("cs")][string]$clientSecret,
+        [Parameter(Mandatory = $true)][Alias("u")][string]$url,
+        [Parameter(Mandatory = $true)][Alias("f")][string]$solutionFolder,
+        [Parameter(Mandatory = $true)][Alias("s")][string]$solution
     )
 	
-    $legacyFolderPath = "$solutionFolder\$solution"
+    Write-Host $pacPath
+    Write-Host $tenantId
+    Write-Host $clientId
+    Write-Host $clientSecret
+    Write-Host $url
+    Write-Host $solutionFolder
+    Write-Host $solution
+
     $pacexepath = "$pacPath\pac.exe"
+    $legacyFolderPath = "$solutionFolder\$solution"
+    $unpackfolderpath = "$solutionFolder\$solution\SolutionPackage"
+
     if (Test-Path "$pacexepath") {
         # Trigger Auth
         Invoke-Expression -Command "$pacexepath auth create --url $url --name ppdev --applicationId $clientId --clientSecret $clientSecret --tenant $tenantId"
-        $unpackfolderpath = "$solutionFolder\$solution\SolutionPackage"
 
         # Trigger Clone or Sync
         $cdsProjPath = "$solutionFolder\$solution\SolutionPackage\$solution.cdsproj"
         $cdsProjFolderPath = "$solutionFolder\$solution\SolutionPackage"
-        if (Test-Path "$cdsProjPath") {
-            $cdsProjfolderPath = [System.IO.Path]::GetDirectoryName("$cdsProjPath")
+
+        if (Test-Path $cdsProjPath) {
+            # $cdsProjfolderPath = [System.IO.Path]::GetDirectoryName("$cdsProjPath")
+            $cdsProjFolderPath = Split-Path -Path $cdsProjPath -Parent
             Set-Location -Path $cdsProjfolderPath
-            $syncCommand = "solution sync --packagetype Both --async"
-            Invoke-Expression -Command "$pacexepath $syncCommand"
+            Invoke-Expression -Command "$pacexepath solution sync --packagetype Both --async"
         }
         else {
-            if (Test-Path "$legacyFolderPath") {
-                Remove-Item "$legacyFolderPath" -recurse -Force
+            if (Test-Path $legacyFolderPath) {
+                Remove-Item $legacyFolderPath -recurse -Force
             }
-            $cloneCommand = "solution clone -n $solution --processCanvasApps $processCanvasApps --outputDirectory ""$unpackfolderpath"" --packagetype Both --async"
-            Invoke-Expression -Command "$pacexepath $cloneCommand"
+            Invoke-Expression -Command "$pacexepath solution clone -n $solution --outputDirectory $unpackfolderpath --packagetype Both --async"
         }
         
         # CleanUp
         If (Test-Path "$cdsProjFolderPath\$solution") {
-            Get-ChildItem -Path "$cdsProjFolderPath\$solution" | Copy-Item -Destination "$cdsProjFolderPath" -Recurse -Container
+            Get-ChildItem -Path "$cdsProjFolderPath\$solution" | Copy-Item -Destination $cdsProjFolderPath -Recurse -Container
             Remove-Item "$cdsProjFolderPath\$solution" -Recurse
         }
     }
