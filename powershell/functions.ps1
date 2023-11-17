cbc26b1e280b84a4e22446ce2f067f0832c7edff
eu <eu@basilevinicius.onmicrosoft.com>
2023-11-17 11:04:59 -0300

    build.yaml

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -1,3 +1,40 @@
+function Set-Pac-Tools-Path {
+    param (
+        [Parameter(Mandatory)] [string]$agentOS
+    )
+
+    if ($agentOS -eq "Linux") {
+        $osFolserSplit = "/"
+        $pacToolsPath = $env:POWERPLATFORMTOOLS_PACCLIPATH + "/pac/tools"
+    }
+    else {
+        $osFolserSplit = "\"
+        $pacToolsPath = $env:POWERPLATFORMTOOLS_PACCLIPATH + "\pac\tools"
+    } 
+
+    Write-Host "##vso[task.setvariable variable=pacPath]$pacToolsPath"
+    Write-Host "##vso[task.setvariable variable=folderSpliter]$osFolserSplit"
+}
+
+function Invoke-Pac-Authenticate {
+    param (
+        [Parameter(Mandatory = $true)][Alias("p")][string]$pacPath,
+        [Parameter(Mandatory = $true)][Alias("t")][string]$tenantId,
+        [Parameter(Mandatory = $true)][Alias("c")][string]$clientId,
+        [Parameter(Mandatory = $true)][Alias("cs")][string]$clientSecret,
+        [Parameter(Mandatory = $true)][Alias("u")][string]$url
+    )
+    if (Test-Path "$pacPath/pac.exe") {
+        $pacexepath = "$pacPath/pac.exe"
+        Invoke-Expression -Command "$pacexepath auth create -n almfpdev -u $url -id $clientId -cs $clientSecret -t $tenantId"
+    }
+    else {
+        Write-Host "pac.exe NOT found"
+    }
+
+    return $pacexepath
+}
+
 function Invoke-Clone-Or-Sync-Solution {
     param (
         [Parameter(Mandatory = $true)][Alias("p")][string]$pacPath,
@@ -19,10 +56,6 @@ function Invoke-Clone-Or-Sync-Solution {
     Write-Host $unpackfolderpath
 
     if (Test-Path "$pacexepath") {
-        # Trigger Auth
-        Invoke-Expression -Command "$pacexepath auth create --url $url --name ppdev --applicationId $clientId --clientSecret $clientSecret --tenant $tenantId"
-
-        # Trigger Clone or Sync
         $cdsProjPath = "$unpackfolderpath/$solution.cdsproj"
         Write-Host $cdsProjPath
         
@@ -30,11 +63,13 @@ function Invoke-Clone-Or-Sync-Solution {
         Write-Host $cdsProjFolderPath
 
         if (Test-Path $cdsProjPath) {
+            Write-Host "Sync"
             $cdsProjfolderPath = [System.IO.Path]::GetDirectoryName("$cdsProjPath")
             Set-Location -Path $cdsProjfolderPath
             Invoke-Expression -Command "$pacexepath solution sync --packagetype Both --async"
         }
         else {
+            Write-Host "clone"
             if (Test-Path $legacyFolderPath) {
                 Remove-Item $legacyFolderPath -recurse -Force
             }
